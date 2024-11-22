c92eb13e86a0ebc198197d05e64467263666ef66
Dhanush Rathod <rathoddhanush15@gmail.com>
2024-11-22 23:24:23 +0530

    commited modified files

--- __temp_pipreqsnb_folder/_pipreqsnb_temp_file.py
+++ __temp_pipreqsnb_folder/_pipreqsnb_temp_file.py
@@ -16,3 +16,33 @@ import tempfile
 import os
 import warnings
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+import whisper
+import ffmpeg
+import tempfile
+import os
+import warnings
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+import whisper
+import ffmpeg
+import tempfile
+import os
+import warnings
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+import whisper
+import ffmpeg
+import tempfile
+import os
+import warnings
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+import whisper
+import ffmpeg
+import tempfile
+import os
+import warnings
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+import whisper
+import ffmpeg
+import tempfile
+import os
+import warnings
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
